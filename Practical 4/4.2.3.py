bc6806dd63eb4a6e0c73e96d3323656b6e879bd3
nehachoudhary7-dev <nehachoudhary8089@gmail.com>
2026-05-02 10:18:25 -0700

    4.2.3.py

---
+++ Practical 4/4.2.3.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Prompt the user for the file name
+file_name = input()
+
+# Load the data
+df = pd.read_csv(file_name)
+
+# write the code..
+
+
+
+# Group by City and calculate total quantity sold
+city_sales = df.groupby('City')['Quantity'].sum()
+
+# Find the city with the highest total quantity sold
+best_city = city_sales.idxmax()
+
+
+# Display the result
+print(f"City sold the most products: {best_city}")
