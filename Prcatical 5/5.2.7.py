b18ab20950eba0c2dce7b9bf4e9f7bb7c123601e
nehachoudhary7-dev <nehachoudhary8089@gmail.com>
2026-05-02 10:55:10 -0700

    5.2.7.py

---
+++ Prcatical 5/5.2.7.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the Titanic dataset
+data = pd.read_csv('Titanic-Dataset.csv')
+
+# Data Cleaning
+data['Age'].fillna(data['Age'].median(), inplace=True)
+data['Embarked'].fillna(data['Embarked'].mode()[0], inplace=True)
+data.drop('Cabin', axis=1, inplace=True)
+
+# Convert categorical features to numeric
+data['Sex'] = data['Sex'].map({'male': 0, 'female': 1})
+data = pd.get_dummies(data, columns=['Embarked'], drop_first=True)
+
+# Write your code here for Box Plot for Age by Pclass
+# Box Plot for Age by Pclass
+
+# Create boxplot grouped by Pclass
+data.boxplot(column='Age', by='Pclass')
+
+# Add title and labels
+plt.title("Age by Pclass")
+plt.xlabel("Pclass")
+plt.ylabel("Age")
+
+# Remove default subtitle
+plt.suptitle('')
+
+# Show the plot
+plt.show()
+
